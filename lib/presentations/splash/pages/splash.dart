dc088f4d82425a0c008b90dca3dd691f8f46cd71
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-10-22 13:10:50 +0200

    redirect to GetStartedPage after SplashPage

--- lib/presentations/splash/pages/splash.dart
+++ lib/presentations/splash/pages/splash.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_2030_flutter/presentations/intro/pages/get_started.dart';
 import '../../../core/configs/assets/app_vectors.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,5 +26,16 @@ class SplashPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const GetStartedPage()),
+      );
+    }
   }
 }
