befb482b863828c90c8f8edf67b5e32c961bae94
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-10-22 13:26:04 +0200

    add images manager class in app_images.dart

---
+++ lib/core/configs/assets/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+  static const String format = '.png';
+
+  static const String cover = '${basePath}ariana_bg_intro$format';
+}
