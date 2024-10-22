e1fe730f0bd63d9d25b8cf93376a57ac45aea8df
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-10-22 13:29:00 +0200

    add stack and container conatining bg image as chidren

--- lib/presentations/intro/pages/get_started.dart
+++ lib/presentations/intro/pages/get_started.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart ';
+import 'package:spotify_2030_flutter/core/configs/assets/app_images.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Get Started Page'),
-      ),
-    );
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(AppImages.cover),
+              fit: BoxFit.fill,
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
