ffab0bb432107337c33a1e6db5df9d9137b9aba7
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-10-22 13:54:03 +0200

    add logo and texts in intro page

--- lib/presentations/intro/pages/get_started.dart
+++ lib/presentations/intro/pages/get_started.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart ';
 import 'package:spotify_2030_flutter/core/configs/assets/app_images.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_2030_flutter/core/configs/assets/app_vectors.dart ';
 
 class GetStartedPage extends StatelessWidget {
-  const GetStartedPage({Key? key}) : super(key: key);
+  const GetStartedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +12,38 @@ class GetStartedPage extends StatelessWidget {
         body: Stack(
       children: [
         Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(AppImages.cover),
-              fit: BoxFit.fill,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.cover),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-        ),
+            child: Column(children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: SvgPicture.asset(
+                  AppVectors.logo,
+                ),
+              ),
+              const Spacer(),
+              const Text(
+                'Enjoy Listening to music',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 21),
+              const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: Colors.grey,
+                  fontSize: 13,
+                ),
+              ),
+            ])),
         Container(
           color: Colors.black.withOpacity(0.15),
         )
