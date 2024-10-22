cbeaabdc685431cc3dbceeda476d3877cbacb9fe
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-10-22 13:10:20 +0200

    add GetSartedPage

---
+++ lib/presentations/intro/pages/get_started.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart ';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Get Started Page'),
+      ),
+    );
+  }
+}
