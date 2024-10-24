59a2289a4419d8721448a1646e84fa802a561373
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-10-24 13:52:33 +0200

    add pattern images in the AooVector manager

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -6,4 +6,6 @@ class AppVectors {
 
   static const String lightModeIcon = '${basePath}light_mode_icon$format';
   static const String darkModeIcon = '${basePath}dark_mode_icon$format';
+  static const String pattern1 = '${basePath}Union$format';
+  static const String pattern2 = '${basePath}Union2$format';
 }
