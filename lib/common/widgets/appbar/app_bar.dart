af4617a3eb18d3a55eab906fb79a5754384945c6
Khaled Akel <kakel.ieu2021@student.ie.edu>
2024-10-24 19:44:20 +0200

    Refactor BasicAppBar widget and improve back button functionality

--- lib/common/widgets/appbar/app_bar.dart
+++ lib/common/widgets/appbar/app_bar.dart
@@ -8,11 +8,21 @@ class BasicAppBar extends StatelessWidget {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: IconButton(
-        icon: const Icon(Icons.arrow_back_ios),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+      leading: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color:
+                Theme.of(context).textTheme.bodyMedium?.color ?? Colors.black,
+            size: 15,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          splashColor: Colors.transparent, // No ripple effect
+          highlightColor: Colors.transparent, // No highlight effect
+        ),
       ),
     );
   }
